4cc8262d9902bc24805671dfccf1005250a3dc6e
kaiqw <kaique.eduardo@hotmail.com>
2020-05-12 21:33:19 -0300

    Desenvovimento do histórico de contas e criação do botão limpar histórico

--- app/src/main/java/br/univali/cc/prog/calculadora/MainActivity.java
+++ app/src/main/java/br/univali/cc/prog/calculadora/MainActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText num1;
     EditText num2;
     TextView resultado;
-    TextView historico;
+    ListView historicoListView;
+    List<String> historico  = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         num1 = findViewById(R.id.etNumero1);
         num2 = findViewById((R.id.etNumero2));
         resultado = findViewById(R.id.tvResultado);
-        historico = findViewById(R.id.tvHistorico);
+        historicoListView = findViewById(R.id.lvHistorico);
 
     }
 
@@ -36,7 +42,10 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Float res = Float.valueOf(num1St) + Float.valueOf(num2St);
             resultado.setText("Resultado: " + res);
-            historico.setText(num1St + " + " + num2St + " = " + res);
+            gravaResultado("+", num1St, num2St, res);
+
+
+
         }
 
 
@@ -52,9 +61,8 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Float res = Float.valueOf(num1St) - Float.valueOf(num2St);
             resultado.setText("Resultado: " + res);
+            gravaResultado("-", num1St, num2St, res);
         }
-
-
     }
     public void divisao (View v){
         resultado.setText("");
@@ -63,17 +71,15 @@ public class MainActivity extends AppCompatActivity {
 
         if (num1St.equals("") || num2St.equals("")){
             Toast.makeText(this, "Por favor, informe os dois valores!", Toast.LENGTH_LONG).show();
+        }else {
+            if (num2St.equals("0")) {
+                resultado.setText("NÃO É POSSIVEL DIVIDIR POR 0");
+            } else {
+                Float res = Float.valueOf(num1St) / Float.valueOf(num2St);
+                resultado.setText("Resultado: " + res);
+                gravaResultado("/", num1St, num2St, res);
+            }
         }
-
-        if (num2St.equals("0")){
-            resultado.setText("NÃO É POSSIVEL DIVIDIR POR 0");
-        }
-        else{
-            Float res = Float.valueOf(num1St) / Float.valueOf(num2St);
-            resultado.setText("Resultado: " + res);
-        }
-
-
     }
     public void multiplicacao (View v){
         resultado.setText("");
@@ -85,8 +91,20 @@ public class MainActivity extends AppCompatActivity {
         }else{
             Float res = Float.valueOf(num1St) * Float.valueOf(num2St);
             resultado.setText("Resultado: " + res);
+            gravaResultado("x", num1St, num2St, res);
+
         }
 
+    }
+    public void limparResultado (View v){
+        historico.clear();
+        historicoListView.setAdapter(null);
+    }
 
+    public void gravaResultado(String op, String n1, String n2, Float r){
+        String texto = n1 + op + n2 + " = " + r;
+        historico.add(texto);
+        ArrayAdapter<String> resArrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, historico);
+        historicoListView.setAdapter(resArrayAdapter);
     }
 }
